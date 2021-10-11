836ec1b5bd795c16baa64f437eaedba313e40086
Arwen <arwensookim@gmail.com>
2021-10-10 23:50:42 -0700

    file updated

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const db = require("./config/keys").mongoURI;
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require('passport');
-
+const path = require('path');
 
 // const User = require("./models/User");
 
@@ -45,3 +45,9 @@ app.use("/api/tweets", tweets);
 const port = process.env.PORT || 5000;
 app.listen(port, () => {console.log(`Listening on port ${port}`)});
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+  }
